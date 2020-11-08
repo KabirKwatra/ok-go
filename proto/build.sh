d8ef41073771701339a58749783e548a1c7cad67
Kabir Kwatra <kabir@kwatra.me>
2020-11-08 03:29:35 -0800

    feat: updated api to v1alpha2

--- proto/build.sh
+++ proto/build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 pushd $(dirname "$0")
-protoc -I. -I./googleapis -I$GOPATH/src --go_out=plugins=grpc:.. googleapis/google/assistant/embedded/v1alpha1/embedded_assistant.proto
+protoc -I. -I./googleapis -I$GOPATH/src --go-grpc_out=.. googleapis/google/assistant/embedded/v1alpha2/embedded_assistant.proto
 popd
